5c170152834ce0080ced18935f9fbe947e0d73b0
sigmagegaok <mer.gegako@gmail.com>
2024-10-12 12:56:31 +0400

    first commit

---
+++ day 02/homework/lesson2.py
@@ -0,0 +1,31 @@
+#neme = "gega"
+#neme არის ცვლადი
+# = არის ცვლადისთვის მნიშნელობის მიმნიჭებელი ციმბოლო
+# "gega" არის ცვლადისთვის მნისვნელობა
+
+surname = "sadaghashvili me age is"
+
+#print(neme)
+# პრინტი ფუნქციას გადაეცემა ეკრანზე გამოსატანი ობიეკტი
+
+neme =" me are gega" #ეს არის str (string) ტიპის ცვლადი
+age = (str(12)  + " " +  "end me height is")         # 12ეს არის int (integer) მთელი რიცხვი
+height = 156 #ეს არის float ტიპის ცვლადი (ათწილადი)
+
+#boolean (bool) ტიპის ცვლადი
+
+knows_programming = True #true ან false
+is_ugly = False #snarecase(უბრალოოდ წესიერი სტილი სტანდარტულად)
+isUgly = False # ჯავასკრიპტული camelasle
+
+#print(neme +" " + surname)
+
+#სტრინგი არის პჭყალებში მაცემული სიმბოლოები
+#print(neme + age)
+
+#print(type(age))
+#print(type(neme))
+#print(type(surname))
+#print(type(height))
+#print(type(knows_programming))
+print (neme + (surname) + " " +str(age) + " " +  str(height))
