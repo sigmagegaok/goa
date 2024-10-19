210d26cedf6ea298a29d642ec4991497bb7f3270
sigmagegaok <mer.gegako@gmail.com>
2024-10-19 19:06:18 +0400

    second commit

---
+++ level 3/homework/level.3.py
@@ -0,0 +1,23 @@
+#nomar 1
+
+num1=45129412
+num2=7898690
+num3=457630970
+print(num1+num2+num3)
+
+#nomer 2
+x=23
+y=32
+print(x*y)
+
+
+
+#nimer 3
+
+
+length=10
+
+width=20
+
+print(length+width)
+print('მართკუთხედის პერიმეტრი უდრის str(30*2) ანუ str(60)') #რადგან print(length+width)=30 )
