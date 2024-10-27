af3caac30fc6f4c0a5f15f23b9667510a2703830
sigmagegaok <mer.gegako@gmail.com>
2024-10-27 11:13:16 +0400

    second commit

---
+++ level 3/clscc work.py
@@ -0,0 +1,18 @@
+# level 3:
+
+
+# 1.  შექმენით ცვლადები რამოდენიმე საკითხზე, სახელი, გვარი, ასაკი, ქვეყანა, საცხოვრებელი ადგილი, საყვარელი მანქანა, საყვარელი ჰობი, საყვარელი ფერი, შემდეგ ამ შექმნილი ცვლადებით ააგეთ წინადადება.
+
+
+
+hi='hi gues me neme wos'
+
+neme=' gega olsow me last_neme wos '
+last_neme=' sadaghashvili end me are '
+old=' 12 old end me cuntre wos'
+contre=' georgian end me faworit car wos '
+love_car=' bentle me hobe wos'
+hobee=' coding end me faworitw coler wos'
+love_coler=' green'
+
+print(hi  + neme + last_neme + old + contre + love_car + hobee + love_coler)
