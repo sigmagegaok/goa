22aa0dc460488f8eb7fb7dd5fe9861b7d17d465f
sigmagegaok <mer.gegako@gmail.com>
2024-11-30 17:15:16 +0400

    second commit

---
+++ level 15/homework/2.py
@@ -0,0 +1,8 @@
+nomber = int(input(" enter nomber :"))
+
+if nomber == 0:
+    print("yeu nomber is zero")
+elif nomber < 0 :
+    print("yeu nomber is negativ")
+elif nomber > 0:
+    print("yeu nomber is positiv")
---
+++ level 15/homework/3.py
@@ -0,0 +1,14 @@
+score = int(input(" enter yeu score  : "))
+if score > 100:
+    print("enter real score plsssssss")
+elif score < 0:
+    print("enter real score  plssssssss")
+
+elif score >= 90:
+    print("yeu grinde A")
+elif score  >= 80:
+    print("yeu grinde B")
+elif score  >= 70:
+    print("yeu grinde C")
+elif score  <= 60:
+    print("yeu grinde D")
