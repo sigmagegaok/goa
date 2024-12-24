a7266d0c2c5dc336e93e4e18248056228e1315b5
sigmagegaok <mer.gegako@gmail.com>
2024-12-24 16:19:49 +0400

    second commit

---
+++ level 21/classwork/1.py
@@ -0,0 +1,7 @@
+list = [8,4,2,6]
+list.remove(2)
+print(len(list)+list.count(6))
+# removima tasala 2
+# len(list) = 4 magram remuvi 2 xdis 3ad
+# da count 6 kidev emdteba
+# 3+1= 4
---
+++ level 21/classwork/2.py
@@ -0,0 +1,14 @@
+nums = [2,4,8,9,5]
+
+
+nums.insert(1,3)
+nums.remove(9)
+nums.insert(0, nums.count(8))
+print(nums[3])
+# lists ematema indexs 1ze 3
+# lists akldema 9
+# listhi inspektit daemata dasathishi 1
+# printi nums 3 gvadlevs = 
+# 2, 3 ,4,8,9,5
+#1,2,3,4,8,9,5
+#mesame aris 4
